f5107f9aa4371fc7f830ef4fcdd7cabe41274cd2
Laurentiu <lcostin40@gmail.com>
2021-10-13 21:46:59 +0200

    Corretto il codice per far uscire fuori i quadrati rossi ed aggiunto commenti

--- js/main.js
+++ js/main.js
@@ -5,23 +5,36 @@ console.log(lista);
 
 //Crea un ciclo di numeri che va da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        const quadro = `<li class="verde"> fizz </li>`;
 
-        lista.innerHTML += quadro;
-
-    } else if (i % 5 === 0) {
-        const quadro = `<li class="giallo"> buzz </li>`;
-
-        lista.innerHTML += quadro;
-
-    } else if (i % 15 === 0) {
+    //Crea un quadrato rosso se il valore i è divisibile per 15    
+    if (i % 15 === 0) {
         const quadro = `<li class="rosso"> fizzBuzz </li>`;
 
         lista.innerHTML += quadro;
+
+        //Crea un quadrato giallo se il valore i è divisibile per 5
+    } else if (i % 5 === 0) {
+        const quadro = `<li class="giallo"> buzz </li>`;
+
+        //inserisce nell'html il quadrato
+        lista.innerHTML += quadro;
+
+        //Crea un quadrato Verde se il valore i è divisibile per 3
+    } else if (i % 3 === 0) {
+        const quadro = `<li class="verde"> fizz </li>`;
+
+        //inserisce nell'html il quadrato
+        lista.innerHTML += quadro;
+
+        //Crea un quadrato blu se i non è divisilibile ner per 15 ne per 3 ne per 5
     } else {
         const quadro = `<li class="celeste"> ${i} </li>`;
 
+        //inserisce nell'html il quadrato
         lista.innerHTML += quadro;
     }
 }
+
+
+
+
